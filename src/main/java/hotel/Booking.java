3f7079b5d528f802008cd81c021ee12ca722a9ea
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-27 13:29:07 +0100

    booking class added and passes tests

---
+++ src/main/java/hotel/Booking.java
@@ -0,0 +1,22 @@
+package hotel;
+
+import room.Bedroom;
+
+public class Booking {
+
+    private Bedroom bedroom;
+    private int nights;
+
+    public Booking(Bedroom bedroom, int nights) {
+        this.bedroom = bedroom;
+        this.nights = nights;
+    }
+
+    public Bedroom getBedroom() {
+        return bedroom;
+    }
+
+    public int getNights() {
+        return nights;
+    }
+}
---
+++ src/test/java/BookingTest.java
@@ -0,0 +1,30 @@
+import hotel.Booking;
+import org.junit.Before;
+import org.junit.Test;
+import room.Bedroom;
+import room.RoomType;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookingTest {
+
+    Booking booking;
+    Bedroom bedroom;
+
+    @Before
+    public void before() {
+        bedroom = new Bedroom(123, RoomType.DOUBLE);
+        booking = new Booking(bedroom, 3);
+    }
+
+    @Test
+    public void hasBedroom() {
+        assertEquals(bedroom, booking.getBedroom());
+    }
+
+    @Test
+    public void hasNights() {
+        assertEquals(3, booking.getNights());
+    }
+
+}
